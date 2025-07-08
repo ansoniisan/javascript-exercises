6eef700851bd49063bcba80528f68e1d2b057a44
ansoniisan <anthonymigo25@gmail.com>
2025-07-08 21:06:55 +0900

    Add repeatString function to complete all tests
    repeatString() was added to complete the tests found in repeatString.spec.js.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
+function repeatString(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
-};
+    let repeatedString = "";
+    let i = 1;
+
+    while (i <= num) {
+        repeatedString += string;
+        i++;
+    }
+
+    return repeatedString;
+}
+
+console.log(repeatString("hey", 3));
+console.log(repeatString("hello", 10));
+console.log(repeatString("hi", 1));
+console.log(repeatString("bye", 0));
+console.log(repeatString('goodbye', -1));
 
 // Do not edit below this line
 module.exports = repeatString;
