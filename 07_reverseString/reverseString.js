aefe3e72c31a284b4708544ec1db8641ef916142
ansoniisan <anthonymigo25@gmail.com>
2025-07-08 21:18:15 +0900

    Add reverseString() to pass all tests in reverseString.spec.js

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+function reverseString(string) {
+    return string
+    .split("")
+    .reverse()
+    .join("")
+}
 
-};
+console.log(reverseString("hello"));
+console.log(reverseString('123! abc! Hello, Odinite.'));
+console.log(reverseString(""));
 
 // Do not edit below this line
 module.exports = reverseString;
