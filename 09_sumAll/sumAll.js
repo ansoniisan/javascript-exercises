03ac17f7ab201f8efd5a665bd032428b5af44698
ansoniisan <anthonymigo25@gmail.com>
2025-07-09 15:36:35 +0900

    Add sumAll() to satisfy all sumAll.spec.js tests

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+function sumAll(num1, num2) {
+    let sum = 0;
+    let start;
+    let end;
 
-};
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+    else if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    if (num1 < num2) {
+        start = num1;
+        end = num2;
+    }
+    else {
+        start = num2;
+        end = num1;
+    }
+
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
+
+}
 
 // Do not edit below this line
 module.exports = sumAll;
