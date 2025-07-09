8ead4844d6ec7e59261754be18298c6170231863
ansoniisan <anthonymigo25@gmail.com>
2025-07-09 12:11:55 +0900

    Add removeFromArray function to pass removeFromArray.spec.js

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
-};
+function removeFromArray(array, ...args) {
+    return array.filter((item) => !args.includes(item));
+}
 
-// Do not edit below this line
+/*
+function removeFromArray(){
+    let argumentArray = Array.from(arguments);
+    console.table(argumentArray)
+    let trueArray = argumentArray[0];
+    console.table(trueArray);
+    let returnArray;
+    
+    for (let i = 1; i <= argumentArray.length; i++) {
+        removeElement = argumentArray[i];
+        console.log(removeElement);
+        returnArray += trueArray.filter((item) => item !== removeElement);
+        }
+
+    return returnArray;
+    }*/
+
+//Do not edit below this line
 module.exports = removeFromArray;
