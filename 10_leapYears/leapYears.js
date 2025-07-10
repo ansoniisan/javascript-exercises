e1cc334b9d73772cdde13b0c63782aa9f41a7eae
ansoniisan <anthonymigo25@gmail.com>
2025-07-10 10:07:25 +0900

    Add leapYears() to satisfy leapYears.spec.js

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //if leap year, return true
+    // leap year = if divisible by 4 && divisible by 100 && divisible by 400
+    if (year % 4 === 0) {
+        if (year % 100 === 0 && year % 400 === 0) {
+            return true;
+        }
+        else if (year % 100 === 0) {
+            return false
+        }
+        return true;
+    }
+    
+    return false;
 };
 
 // Do not edit below this line
