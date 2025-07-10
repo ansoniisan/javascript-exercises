7964a5d60f059d827a449a368c638caaf5d60946
ansoniisan <anthonymigo25@gmail.com>
2025-07-10 10:48:29 +0900

    Add convertToCelsius() and convertToFahrenheit() to satisfy tempConversion.spec.js

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (number) {
+  let celsiusConversionFactor = 5 / 9;
+  let intermediateCelsiusConversion = (number - 32) * celsiusConversionFactor;
+
+  if (!Number.isInteger(intermediateCelsiusConversion)) {
+    return parseFloat(intermediateCelsiusConversion.toFixed(1));
+  };
+  return intermediateCelsiusConversion;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (number) {
+  let fahrenheitConversionFactor = 9 / 5;
+  let intermediateFahrenheitConversion = number * fahrenheitConversionFactor + 32;
+
+  if (!Number.isInteger(intermediateFahrenheitConversion)) {
+    return parseFloat(intermediateFahrenheitConversion.toFixed(1));
+  }
+  return intermediateFahrenheitConversion;
 };
 
 // Do not edit below this line
