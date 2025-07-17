e65dfc7b5379881ce07d77cd7d0a909e7ada1603
ansoniisan <anthonymigo25@gmail.com>
2025-07-17 16:50:30 +0900

    Add ()s to complete all tests in calculator.spec.js

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,24 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((sum, current) => sum * current, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  let product = 1;
+
+  for (let i = num; i > 0; i--) {
+    product *= i;
+  }
+
+  return product;
 	
 };
 
