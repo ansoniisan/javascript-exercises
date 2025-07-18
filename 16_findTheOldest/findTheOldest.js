2566865151713110abc0f1cef8fe0738396d640f
ansoniisan <anthonymigo25@gmail.com>
2025-07-18 13:56:48 +0900

    Add findTheOldest() to pass all tests in findTheOldest.spec.js

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const year = new Date().getFullYear();
+    
+    //Create new array and add age property to avoid mutating original array
+    const orderedArray = arr
+    .map((person) => ({
+        ...person,
+        age: (person.yearOfDeath || year) - person.yearOfBirth,
+    }))
+    .sort((a, b) => b.age - a.age);
+    
+    return orderedArray[0];
 };
 
 // Do not edit below this line
